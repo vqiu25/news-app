a40412cdb9163e054fd6ecdfd73aa2ab61b4eb23
Victor Esther Qiu <vqiu25@gmail.com>
2025-04-01 11:09:25 +1300

    ✨ complete header row

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import CategorySlider from "../components/home/CategorySlider";
 import Colour from "../shared/Colour";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 function Home() {
   return (
     <View>
-      <Text style={styles.appName}>News</Text>
+      <View style={styles.headerRow}>
+        <Text style={styles.appName}>News</Text>
+        <Ionicons name="notifications-outline" size={24} color="black" />
+      </View>
       <CategorySlider />
     </View>
   );
@@ -17,6 +21,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: Colour.primary,
+  },
+  headerRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
 
