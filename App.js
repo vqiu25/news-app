2272425278a4496938e61430dbf78dd1c74eefa6
Victor Esther Qiu <vqiu25@gmail.com>
2025-04-01 11:03:02 +1300

    🎨 add padding

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { SafeAreaView, StyleSheet, Platform, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Platform,
+  Text,
+  View,
+} from "react-native";
 import Home from "./app/screens/Home";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Home />
+      <View style={styles.paddingContainer}>
+        <Home />
+      </View>
     </SafeAreaView>
   );
 }
@@ -13,6 +22,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 20 : 20,
+  },
+  paddingContainer: {
+    padding: 20,
   },
 });
